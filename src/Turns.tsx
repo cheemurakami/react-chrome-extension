57fc88885b1ecdd98e1ddccc42b3f490af2dc7e6
Chisato Murakami <kiwipom1031@gmail.com>
2024-01-08 16:35:43 -0800

    add styled components

--- src/Turns.tsx
+++ src/Turns.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent } from 'react'
 import Names from './Names.tsx'
+import styled from "styled-components";
 
 const Turns: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>('')
@@ -14,16 +15,53 @@ const Turns: React.FC = () => {
   return (
     <>
       <h2>Turns</h2>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input type='text' placeholder='Type name' onChange={(e) => setInputValue(e.target.value)} value={inputValue} />
-          <button type='submit'>Add</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <InputContainer>
+          <InputDiv>
+            <Input type='text' placeholder='Type name' onChange={(e) => setInputValue(e.target.value)} value={inputValue} />
+          </InputDiv>
+          <ButtonDiv>
+            <Button type='submit'>Add</Button>
 
-      </div>
+          </ButtonDiv>
+        </InputContainer>
+      </form>
+
       {nameList.length > 0 && <Names nameList={nameList} />}
     </>
   )
 }
+
+const InputContainer = styled.div`
+align-items: center;
+display: flex;
+justify-content: center;
+
+@media (max-width: 600px) {
+    flex-direction: column;
+  }
+`
+
+const InputDiv = styled.div`
+`
+
+const ButtonDiv = styled.div`
+`
+
+const Button = styled.button`
+  background-color: #91E5F6;
+  color: white;
+  margin-bottom: 8px;
+  margin-top: 8px;
+`
+
+const Input = styled.input`
+  background-color: white;
+  border: 1px solid gray;
+  color: black;
+  font-size: 16px;
+  height: 40px;
+  margin-right: 20px;
+`
 
 export default Turns
